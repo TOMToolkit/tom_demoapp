d2591c9dcbb3eebe4fc56bfbe5b3dc0f434b09cb
Joey Chatelain <jchate6@gmail.com>
2024-12-19 21:01:36 -0800

    establish demo app profile with integration points

---
+++ tom_demoapp/templatetags/demo_extras.py
@@ -0,0 +1,28 @@
+from django import template
+from django.forms.models import model_to_dict
+
+from tom_demoapp.models import DemoProfile
+
+register = template.Library()
+
+
+@register.inclusion_tag('tom_demoapp/partials/profile_demo.html')
+def demo_profile_data(user):
+    """
+    Returns the app specific user information as a dictionary to be used in the context of the above partial.
+    """
+
+    exclude_fields = ['user', 'id']
+    try:
+        demo_profile_dict = model_to_dict(user.demoprofile, exclude=exclude_fields)
+        demo_profile_dict['demo_secret'] = "**************"
+        return {
+            'user': user,
+            'demo_profile': user.demoprofile,
+            'demo_profile_data': demo_profile_dict,
+        }
+    except DemoProfile.DoesNotExist:
+        DemoProfile.objects.create(user=user)
+        return {'user': user,
+                'demo_profile': user.demoprofile,
+                'demo_profile_data': {}}
--- tom_demoapp/urls.py
+++ tom_demoapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from tom_demoapp.views import DemoView
+from tom_demoapp.views import DemoView, ProfileUpdateView
 
 
 app_name = 'tom_demoapp'
@@ -8,4 +8,5 @@ app_name = 'tom_demoapp'
 urlpatterns = [
     path('<int:pk>/demo', DemoView.as_view(), name='demo-page'),
     path('', DemoView.as_view(), name='demo-page'),
+    path('users/<int:pk>/update/', ProfileUpdateView.as_view(), name='demo-profile-update'),
 ]

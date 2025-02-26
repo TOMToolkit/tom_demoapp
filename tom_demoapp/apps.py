20ef2281a2a19871ff78c70ea0b849fadf1e8764
Joey Chatelain <jchate6@gmail.com>
2025-02-25 16:24:49 -0800

    revert last commit

--- tom_demoapp/apps.py
+++ tom_demoapp/apps.py
@@ -18,9 +18,9 @@ class TomDemoappConfig(AppConfig):
         - 'text': The text of the button
         """
         return {'namespace': f'{self.label}:demo-page',
-                'title': f'{self.verbose_name} Target Button',
+                'title': f'{self.label} Target Button',
                 'class': 'btn  btn-danger',
-                'text': self.verbose_name,
+                'text': self.label,
                 }
 
     def nav_items(self):

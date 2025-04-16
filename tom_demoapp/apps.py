44a0fad223a615f11a9d8a180a171dc686092f03
Joey Chatelain <jchate6@gmail.com>
2025-04-16 16:28:58 -0700

    fix docstring typo

--- tom_demoapp/apps.py
+++ tom_demoapp/apps.py
@@ -28,7 +28,7 @@ class TomDemoappConfig(AppConfig):
         Integration point for adding items to the navbar.
         This method should return a list of dictionaries that include a `partial` key pointing to the html templates to
         be included in the navbar. The `position` key, if included, should be either "left" or "right" to specify which
-        side of the navbar the partial should be included on. If not included, a right side nav item is assumed.
+        side of the navbar the partial should be included on. If not included, a left side nav item is assumed.
         """
         # TODO: These filenames probably don't need 'demo' in them b/c they're namespaced in the app folder
         return [{'partial': f'{self.name}/partials/navbar_demo.html', 'position': 'right'},

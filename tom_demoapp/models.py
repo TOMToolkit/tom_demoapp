e57905bf4e13f04a730ac9c047f55dcfb0aff27b
William (Lindy) Lindstrom <llindstrom@lco.global>
2026-05-29 15:20:48 -0700

    update for new encryptption API

--- tom_demoapp/models.py
+++ tom_demoapp/models.py
@@ -1,14 +1,14 @@
-from django.db import models
 from django.contrib.auth.models import User
-from tom_common.models import EncryptedProperty
+from django.db import models
+
+from tom_common.encryption import EncryptedModelField
 
 
 class DemoProfile(models.Model):
-    """Demo Profile model for a TOMToolkit User"""
+    """Demo Profile model for a TOMToolkit User."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     demo_field = models.CharField(max_length=100, null=True, blank=True, verbose_name='Demo Field')
-    _demo_secret_encrypted = models.BinaryField(null=True, blank=True)  # ciphertext (private)
-    demo_secret = EncryptedProperty('_demo_secret_encrypted')               # descriptor (public)
+    demo_secret = EncryptedModelField(null=True, blank=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.user.username} Demo App Profile'

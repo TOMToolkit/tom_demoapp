535a0db89f6e113756eb5b6732a9bc297aa7830e
Joey Chatelain <jchate6@gmail.com>
2026-06-12 15:58:27 -0700

    update bootstrap

--- tom_demoapp/tests/boot_django.py
+++ tom_demoapp/tests/boot_django.py
@@ -6,8 +6,8 @@
 import os
 import django
 from django.conf import settings
+from tom_common.default_settings import TOMTOOKIT_INSTALLED_APPS, TOMTOOKIT_MIDDLEWARE
 
-# TODO: Change this to the name of the app you are testing
 APP_NAME = 'tom_demoapp'  # the stand-alone app we are testing
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), APP_NAME))
@@ -26,31 +26,8 @@ def boot_django():
             }
         },
         TOM_NAME='Test TOM',
-        INSTALLED_APPS=(
-            'django.contrib.admin',
-            'django.contrib.auth',
-            'django.contrib.contenttypes',
-            'django.contrib.sessions',
-            'django.contrib.messages',
-            'django.contrib.staticfiles',
-            'django.contrib.sites',
-            'django_extensions',
-            'guardian',
-            'tom_common',
-            'django_comments',
-            'bootstrap4',
-            'crispy_forms',
-            'rest_framework',
-            'rest_framework.authtoken',
-            'django_filters',
-            'django_gravatar',
-            'tom_targets',
-            'tom_alerts',
-            'tom_catalogs',
-            'tom_observations',
-            'tom_dataproducts',
-            APP_NAME,  # defined above
-        ),
+        INSTALLED_APPS=TOMTOOKIT_INSTALLED_APPS+[APP_NAME],
+        SITE_ID=1,
         EXTRA_FIELDS={},
         TIME_ZONE='UTC',
         USE_TZ=True,
@@ -59,18 +36,7 @@ def boot_django():
             'observation_change_state': 'tom_common.hooks.observation_change_state',
             'data_product_post_upload': 'tom_dataproducts.hooks.data_product_post_upload'
         },
-        MIDDLEWARE=[
-            'django.middleware.security.SecurityMiddleware',
-            'django.contrib.sessions.middleware.SessionMiddleware',
-            'django.middleware.common.CommonMiddleware',
-            'django.middleware.csrf.CsrfViewMiddleware',
-            'django.contrib.auth.middleware.AuthenticationMiddleware',
-            'django.contrib.messages.middleware.MessageMiddleware',
-            'django.middleware.clickjacking.XFrameOptionsMiddleware',
-            'tom_common.middleware.Raise403Middleware',
-            'tom_common.middleware.ExternalServiceMiddleware',
-            'tom_common.middleware.AuthStrategyMiddleware',
-        ],
+        MIDDLEWARE=TOMTOOKIT_MIDDLEWARE,
         TEMPLATES=[
             {
                 'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -96,6 +62,9 @@ def boot_django():
         STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')],
         MEDIA_ROOT=os.path.join(BASE_DIR, 'data'),
         MEDIA_URL='/data/',
+        TOM_ALERT_CLASSES=[
+            'tom_antares.antares.ANTARESBroker',
+        ],
         FACILITIES={
             'LCO': {
                 'portal_url': 'https://observe.lco.global',
